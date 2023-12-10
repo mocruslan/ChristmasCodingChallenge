d3120b016be8b6250160d6bc9317827596ac323b
ruslan <mochulskyy.ruslan@gmail.com>
2023-12-10 22:50:51 +0100

    Init commit

---
+++ src/main/java/com/mocruslan/Main.java
@@ -0,0 +1,9 @@
+package com.mocruslan;
+
+import com.mocruslan.days.DayOne;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
